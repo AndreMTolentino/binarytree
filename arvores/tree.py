7b0493db05747570db75b4305918f6102fcfb8c6
André Tolentino <andretolentino@Andres-MacBook-Pro.local>
2024-09-06 00:03:42 -0300

    lesson2: function postorder_example_tree and height

--- arvores/tree.py
+++ arvores/tree.py
@@ -23,53 +23,24 @@ class BinaryTree:
             self.simetric_traversal(node.right)
             print(')', end = '')
 
-if __name__ == "__main__":
-    
-    # n1 = Node(7)
-    # n2 = Node(18)
-    # n3 = Node(14)
- 
-    # n1.left = n2
-    # n1.right = n3
-    # tree = BinaryTree(n1)
+    def postorder_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left is not None:
+            self.postorder_traversal(node.left)
+        if node.right is not None:
+            self.postorder_traversal(node.right)
+        print(node)
 
-    #      '7'
-    #    /     \
-    #  '18'      '14'
-
-    tree = BinaryTree()
-    n1 = Node('a')
-    n2 = Node('+')
-    n3 = Node('*')
-    n4 = Node('b')
-    n5 = Node('-')
-    n6 = Node('/')
-    n7 = Node('c')
-    n8 = Node('d')
-    n9 = Node('e')
-
-    n6.left = n7
-    n6.right = n8
-    n5.left = n6
-    n5.right = n9
-    n3.left = n4
-    n3.right = n5
-    n2.left = n1
-    n2.right = n3
-    
-    tree.root = n2
-
-    tree.simetric_traversal()
-    # print()
-
-    #      '+'
-    #    /     \
-    #  'a'      '*'
-    #          /   \
-    #        'b'    '-'
-    #              /    \
-    #            '/'    'e' 
-    #           /   \
-    #         'c'   'd'
-
-    # (a + (b * ((c/d) - e)))
+    def height(self, node=None):
+        if node is None:
+            node = self.root
+        hleft = 0
+        hright = 0
+        if node.left:
+            hleft = self.height(node.left)
+        if node.right:
+            hright = self.height(node.right)
+        if hright > hleft:
+            return hright + 1
+        return hleft + 1
